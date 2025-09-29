d2f045d660fa2b3a4396abb55ef875738670cb1b
RochevYaroslav <justmisterstone@gmail.com>
2025-09-29 11:09:13 +0500

    Task5

--- Tyuiu.RochevYA.Sprint2.Task5.V2.Lib/DataService.cs
+++ Tyuiu.RochevYA.Sprint2.Task5.V2.Lib/DataService.cs
@@ -10,19 +10,19 @@ namespace Tyuiu.RochevYA.Sprint2.Task5.V2.Lib
                 case 0:
                 case 1:
                 case 11:
-                    return "Зима";
+                    return "зима";
                 case 2:
                 case 3:
                 case 4:
-                    return "Весна";
+                    return "весна";
                 case 5:
                 case 6:
                 case 7:
-                    return "Лето";
+                    return "лето";
                 case 8:
                 case 9:
                 case 10:
-                    return "Осень";
+                    return "осень";
                 default:
                     return "None";
             }
