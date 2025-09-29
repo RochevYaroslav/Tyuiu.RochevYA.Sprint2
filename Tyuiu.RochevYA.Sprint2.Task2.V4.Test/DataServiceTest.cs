4af73ac0213826641b5e056526e773c05bf3fe9f
RochevYaroslav <justmisterstone@gmail.com>
2025-09-29 10:23:50 +0500

    Task2

---
+++ Tyuiu.RochevYA.Sprint2.Task2.V4.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.RochevYA.Sprint2.Task2.V4.Lib;
+namespace Tyuiu.RochevYA.Sprint2.Task2.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual(true, ds.CheckDotInShadedArea(3, 3));
+            Assert.AreEqual(false, ds.CheckDotInShadedArea(1, 1));
+            Assert.AreEqual(true, ds.CheckDotInShadedArea(8, 8));
+        }
+    }
+}
---
+++ Tyuiu.RochevYA.Sprint2.Task2.V4/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.RochevYA.Sprint2.Task2.V4.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("X: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Y: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(ds.CheckDotInShadedArea(x, y));
+        Console.WriteLine("Сделал Рочев Ярослав Антонович | ИБКСб-25-1");
+    }
+}
